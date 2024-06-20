79bab9bae79d9f0691b78744312fc11487a0bbe7
Johane Dingding <your.email@example.com>
2024-06-20 10:22:39 +0800

    change program.cs dll files

--- MedMinder_Api/Program.cs
+++ MedMinder_Api/Program.cs
@@ -1,38 +1,45 @@
-using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MedMinder_Api.Data;
 using MedMinder_Api.Controllers;
 using Newtonsoft.Json.Serialization;
+using AutoMapper;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// builder.Services.AddControllers().AddNewtonsoftJson(s => {
-//     s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-// });
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 });
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// var sqlConBuilder = new SqlConnectionStringBuilder();
-// sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
-// builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
-
-
+// Configure DbContext (using InMemoryDatabase for Development)
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase(databaseName: "MedMinderDb"));
 
+// Register services and filters
 builder.Services.AddScoped<IPatientRepo, PatientRepo>();
 builder.Services.AddScoped<TestAsyncActionFilter>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
+// Enable CORS middleware
+app.UseCors("AllowOrigin");
 
 if (app.Environment.IsDevelopment())
 {
@@ -41,7 +48,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
